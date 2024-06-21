32a9c75ca1a81067016e3f9c6d74829426544a82
Rituparna Curt <rituparna50@gmail.com>
2024-06-21 13:04:22 -0700

    Python script for SVMs

---
+++ Support_Vector_Machines.py
@@ -0,0 +1,53 @@
+## Support Vector Machines (SVM)
+
+'''Supervised machine learning 
+    
+    These are powerful algorithms for classification and regression tasks. They find the optimal hyperplane that 
+    best separates data points in differnet classes.Best known for their effectiveness in classification problems. 
+    
+SVM aims to find the best boundary or hyperplane that separates different classes of data points with the maximum margin. 
+In otehr words, SVM tries to find the line (in 2D), plane (in 3D), or hyperplane (in higher dimensions)
+that best separates the classes of data
+
+Support Vectors:
+
+Support vectors are the data points that are closest to the hyperplane.
+These points are critical because they define the position and orientation of the hyperplane.
+The hyperplane is determined by these points, and not by the points farther away.
+'''
+
+''' In the Iris dataset, if we are trying to classify the species of the flower based on sepal and petal measurements,
+a linear SVM would find the line that best separates the different species.'''
+     
+
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+#Load the iris data 
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+#Split the data into traininf and testing sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=None, random_state=42)
+
+#Train a Support vector Machine classifier 
+svm_clf = SVC(kernel='linear', C=1)
+svm_clf.fit(X_train, y_train)
+
+#Make predictions on the test set 
+y_pred = svm_clf.predict(X_test)
+
+#Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Support Vector machines accuracy: {accuracy}")
+
+## Accuracy = (Nunber of Correct Predictions) / (Total number of Predictions)
+
+# Save accuracy to the accuracies.md file 
+with open("/Users/stanford/Desktop/FDL Prep/Accuracies.md", "a") as f:
+    f.write("# Support Vector Machines (SVM)\n")
+    f.write(f"Accuracy: {accuracy}\n\n")
